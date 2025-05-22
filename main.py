6fe96dc68dd81d51ff186514e9cbe1b549fea840
JackSmiles <jackmozillq@gmail.com>
2025-05-22 17:15:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Create by a kid with age is 14 years old XD
+# My name is Jack and this is my firts code !
+# ¡¡¡¡ IMPORTANT !!!! change the name for your girlfriend or your boyfried ¡¡¡¡ IMPORTANT !!!!
+
+count = 0 # Start a count
+name = "Kharla" # You can change the name
+
+while True: # Create a loop
+    count += 1 # Plus the count (example += 1 is sum)
+    print(count, f"- I love you so much {name}") # Import the message "I love you" with count, the int with str is can't start
+    if count == 99: # if count is 99 so
+        print("I love you 100") # import the message "I love you 101"
+        break # Finish the loop
